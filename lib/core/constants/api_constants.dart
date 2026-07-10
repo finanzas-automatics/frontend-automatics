152be1568a6dc65d897fdd28dcd690d52fb8f709
kcc12321 <130299015+kcc12321@users.noreply.github.com>
2026-07-10 10:48:03 -0500

    Update base URL for API constants

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -3,6 +3,6 @@ import 'package:flutter/foundation.dart';
 class ApiConstants {
   // Use the deployed backend URL
   static String get baseUrl {
-    return 'https://backend-automatics-final-production.up.railway.app/api';
+    return 'https://backend-automatics-final-production-fc18.up.railway.app/api';
   }
 }
